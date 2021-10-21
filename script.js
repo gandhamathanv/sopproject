25ceb512c3aa2f3a1fe351e5f83d43b62e443d50
gandhamathan <gandhamathanv.20cse@kongu.edu>
2021-10-21 15:57:21 +0530

    initial file added

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+function submit() {
+    const name = document.querySelector(`.name`).value;
+    const rollNumber = document.querySelector(`.roll`).value;
+}
+document.querySelector(".submit").addEventListener("click", submit);
