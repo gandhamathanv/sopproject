4aa934d9f3862c9473116ee0f4c116e3ced2f38b
Gandhamathan V <92502489+gandhamathanv@users.noreply.github.com>
2021-10-30 20:20:12 +0530

    hadgjkpo

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "sopprotocol";
+$host = "sql6.freesqldatabase.com";
+$user = "sql6447894";
+$password = "CE3N52fdqM";
+$db = "sql6447894";
 
 $conn = mysqli_connect($host, $user, $password, $db);
 
